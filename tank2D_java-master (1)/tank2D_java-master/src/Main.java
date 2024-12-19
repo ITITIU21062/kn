56507bb802677552d27701a900056123c63e139e
ITITIU21062 <118947672+ITITIU21062@users.noreply.github.com>
2024-12-19 09:39:17 +0700

    Update Main.java

--- tank2D_java-master (1)/tank2D_java-master/src/Main.java
+++ tank2D_java-master (1)/tank2D_java-master/src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 		JFrame obj = new JFrame();
 		Gameplay gamePlay = new Gameplay();
 		
-		obj.setBounds(10, 10, 800, 630); // Đặt kích thước cửa sổ game
-		obj.setTitle("Tank 2D"); // Tiêu đề của cửa sổ game
-		obj.setBackground(Color.gray); // Màu nền của cửa sổ
+		obj.setBounds(10, 10, 800, 630); // set kích thước cửa sổ game
+		obj.setTitle("Tank 2D"); // đặt tiêu đề cửa sổ game
+		obj.setBackground(Color.gray); // set Màu nền
 		obj.setResizable(false); // Không cho phép thay đổi kích thước cửa sổ
 		
-		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Đóng ứng dụng khi tắt cửa sổ
-		obj.add(gamePlay); // Thêm đối tượng Gameplay (nơi xử lý logic game) vào JFrame
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Đóng ứng dụng bằng cái nút đỏ
+		obj.add(gamePlay); // bỏ gamePlay JFrame
 		obj.setVisible(true); // Hiển thị cửa sổ game
 
 	}
