84f42e304ac18897bcb9e77973092c39ced0db62
ITITIU21062 <118947672+ITITIU21062@users.noreply.github.com>
2024-12-20 00:49:31 +0700

    Update brick.java

--- tank2D_java-master (1)/tank2D_java-master/src/brick.java
+++ tank2D_java-master (1)/tank2D_java-master/src/brick.java
@@ -6,29 +6,29 @@ import javax.swing.ImageIcon;
 
 
 public class brick {
-	int bricksXPos[] = {50,350,450,550,50,300,350,450,550,150,150,450,550,
+	int bricksXPos[] = {50,350,450,550,50,300,350,450,550,150,150,450,550,                                  // tọa độ x của cục gạch
 						250,50,100,150,550,250,350,450,550,50,250,350,550,
 						50,150,250,300,350,550,50,150,250,350,450,550,50,
 						250,350,550};
 	
-	int bricksYPos[] = {50,50,50,50,100,100,100,100,100,150,200,200,200,250,
+	int bricksYPos[] = {50,50,50,50,100,100,100,100,100,150,200,200,200,250,                                // tọa ộ y của cục gạch
 						300,300,300,300,350,350,350,350,400,400,400,400,450,
 						450,450,450,450,450,500,500,500,500,500,500,550,550,
 						550,550};
 	
-	int solidBricksXPos[] = {150,350,150,500,450,300,600,400,350,200,0,200,500};
+	int solidBricksXPos[] = {150,350,150,500,450,300,600,400,350,200,0,200,500};                            // tọa độ x của cục gạch không bể đc
 	
-	int solidBricksYPos[] = {0,0,50,100,150,200,200,250,300,350,400,400,450};
+	int solidBricksYPos[] = {0,0,50,100,150,200,200,250,300,350,400,400,450};                              // tọa độ x của cục gạch không bể đc
 	
-	int brickON[] = new int[42];
+	int brickON[] = new int[42];                                                                           // trạng thái của cục gạch (1 là còn 0 là bể)
 	
 	private ImageIcon breakBrickImage;
 	private ImageIcon solidBrickImage;
 	
 	public brick()
 	{
-		breakBrickImage=new ImageIcon("break_brick.jpg");
-		solidBrickImage=new ImageIcon("solid_brick.jpg");	
+		breakBrickImage=new ImageIcon("break_brick.jpg");           // hình cục gạch bể dc
+		solidBrickImage=new ImageIcon("solid_brick.jpg");           // hình cục gạch không bể dc
 		
 		for(int i=0; i< brickON.length;i++)
 		{
@@ -38,7 +38,7 @@ public class brick {
 	
 	public void draw(Component c, Graphics g)
 	{
-		for(int i=0; i< brickON.length;i++)
+		for(int i=0; i< brickON.length;i++)                                                      // nếu cục gạch có status 1 thì vẽ nó
 		{
 			if(brickON[i]==1)
 			{
@@ -51,21 +51,21 @@ public class brick {
 	{
 		for(int i=0; i< solidBricksXPos.length;i++)
 		{			
-			solidBrickImage.paintIcon(c, g, solidBricksXPos[i],solidBricksYPos[i]);
+			solidBrickImage.paintIcon(c, g, solidBricksXPos[i],solidBricksYPos[i]);        // vẽ cục gạch không thể bể
 		}
 	}
 	
 	public boolean checkCollision(int x, int y)
 	{
 		boolean collided = false;
-		for(int i=0; i< brickON.length;i++)
+		for(int i=0; i< brickON.length;i++)                            // kiểm tra tất cả viên gạch
 		{
-			if(brickON[i]==1)
+			if(brickON[i]==1)                    // nếu viên gạch đó trạng thái là 1(chưa bể)
 			{
-				if(new Rectangle(x, y, 10, 10).intersects(new Rectangle(bricksXPos[i], bricksYPos[i], 50, 50)))
-				{
-					brickON[i] = 0;
-					collided = true;
+				if(new Rectangle(x, y, 10, 10).intersects(new Rectangle(bricksXPos[i], bricksYPos[i], 50, 50)))   // cái intersect là để coi 2 hình cn có giao không
+				{                                                                                                 // tức là kiểm tra cục gạch có va với đạn không
+					brickON[i] = 0;         // nếu có thì status cục gạch về 0 (bể)
+					collided = true;        // chuyển trạng thái collided (va chạm) thành có
 					break;
 				}
 			}
@@ -81,7 +81,7 @@ public class brick {
 		{		
 			if(new Rectangle(x, y, 10, 10).intersects(new Rectangle(solidBricksXPos[i], solidBricksYPos[i], 50, 50)))
 			{		
-				collided = true;
+				collided = true;               // gạch khoong bể, đạn bắn kệ
 				break;
 			}			
 		}
