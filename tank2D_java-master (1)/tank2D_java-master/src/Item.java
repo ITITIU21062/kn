73f2ac04a95f340357bded911dff015a36ad6be8
ITITIU21062 <118947672+ITITIU21062@users.noreply.github.com>
2024-12-23 01:27:56 +0700

    Update Item.java

--- tank2D_java-master (1)/tank2D_java-master/src/Item.java
+++ tank2D_java-master (1)/tank2D_java-master/src/Item.java
@@ -6,17 +6,17 @@ import java.util.Random;
 import javax.swing.ImageIcon;
 
 public class Item {
-    private int itemsXPos[];
+    private int itemsXPos[];          // tọa độ của item
     private int itemsYPos[];
-    private boolean itemsActive[];
-    private ImageIcon itemImages[];
-    private int itemTypes[];
+    private boolean itemsActive[];    // đã bị húc chưa
+    private ImageIcon itemImages[];   // khai báo mảng để lưu item và hình của nó
+    private int itemTypes[];          // iteam gì?
     private Random random;
 
     public Item(int mapWidth, int mapHeight, int brickSize, int[][] occupiedPositions) {
         random = new Random();
-        itemImages = new ImageIcon[1];
-        itemImages[0] = new ImageIcon(new ImageIcon("tnt.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));
+        itemImages = new ImageIcon[1];     // chỉ dùng mỗi tnt nên khai 1 thôi
+        itemImages[0] = new ImageIcon(new ImageIcon("tnt.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));               // tnt
         //itemImages[1] = new ImageIcon(new ImageIcon("heal.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));
         //itemImages[2] = new ImageIcon(new ImageIcon("snowflake.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH));
         
@@ -41,7 +41,7 @@ public class Item {
         }
     }
 
-    private boolean isOccupied(int x, int y, int[][] occupiedPositions) {
+    private boolean isOccupied(int x, int y, int[][] occupiedPositions) {    // coi chỗ đó có gạch không
         for (int[] pos : occupiedPositions) {
             if (pos[0] == x && pos[1] == y) {
                 return true; // Vị trí bị chiếm bởi gạch hoặc vật cản khác
@@ -50,7 +50,7 @@ public class Item {
         return false;
     }
 
-    public void draw(Component c, Graphics g) {
+    public void draw(Component c, Graphics g) {                // vẽ item
         for (int i = 0; i < itemsXPos.length; i++) {
             if (itemsActive[i]) {
                 itemImages[itemTypes[i]].paintIcon(c, g, itemsXPos[i], itemsYPos[i]);
@@ -58,12 +58,12 @@ public class Item {
         }
     }
 
-    public int checkCollision(int x, int y) {
+    public int checkCollision(int x, int y) {              // xem xe tăng có đâm item ko
         for (int i = 0; i < itemsXPos.length; i++) {
             if (itemsActive[i] &&
                 new Rectangle(x, y, 30, 30).intersects(new Rectangle(itemsXPos[i], itemsYPos[i], 30, 30))) {
                 itemsActive[i] = false; // Item bị nhặt
-                return itemTypes[i]; // Return the type of item collected
+                return itemTypes[i]; // trả về loại item bị nhặt, nhưng có vẻ bug
             }
         }
         return -1; // No collision
